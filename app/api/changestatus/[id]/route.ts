22ec40542d28dca898b0f1fdf82bcdaa9e71113f
Amrut Deshpande <amrutdeshpande2004@gmail.com>
2026-06-18 12:21:07 +0530

    Update route.ts

--- app/api/changestatus/[id]/route.ts
+++ app/api/changestatus/[id]/route.ts
@@ -1,8 +1,8 @@
 import { User } from "@/app/context/UserContext/user-context";
 import { prisma } from "@/prisma/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function PUT(req: NextResponse, {params} : {params : Promise<{id : string}>}) {
+export async function PUT(req: NextRequest, {params} : {params : Promise<{id : string}>}) {
     const {id} = await params
     const {status} = await req.json()
     const user = await User()
